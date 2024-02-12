b972db1eb37f50f2b387ca4947495827934ab1ff
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2024-02-12 06:01:21 +0400

    v0.0.3

---
+++ assets/js/functions.js
@@ -0,0 +1,24 @@
+function OpenProjectView()
+{
+    alert("yes");
+}
+/*
+const myCarousel = document.getElementById('carousel-1')
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+     //StopVideoPlaybacks();
+})
+
+function StopVideoPlaybacks() 
+{
+	var videos = document.querySelectorAll('iframe, video');
+	Array.prototype.forEach.call(videos, function (video) {
+		if (video.tagName.toLowerCase() === 'video') {
+			video.stop();
+		} else {
+            console.log(video);
+			var src = video.src;
+			video.src = src;
+		}
+	});
+};*/
